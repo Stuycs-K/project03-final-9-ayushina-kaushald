296c5811b229e4e624a7bd2147d6209b15534cf4
Arslan Ayushin <aayushin40@stuy.edu>
2024-01-04 19:59:21 -0500

    add function write word to file

--- words.c
+++ words.c
@@ -1,7 +1,12 @@
 #include "words.h"
 
 void add_word(char *word) {
+    char *buff = malloc(BUFFER_SIZE);
+    strncpy(buff, word, BUFFER_SIZE - 1);
+    buff[BUFFER_SIZE-1] = 0;
 
+    int file = open(FILE_NAME, O_WRONLY | O_CREAT, 0644);
+    int wbytes = write(file, buff, BUFFER_SIZE);
 }
 
 int find_word(char *word) {
