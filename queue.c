052dd2f15b4401ad34d8877a4a1ddd2b0486cf1e
Arslan Ayushin <aayushin40@stuy.edu>
2024-01-05 22:44:10 -0500

    added create_queue fixed h file

--- queue.c
+++ queue.c
@@ -1,7 +1,13 @@
 #include "queue.h"
 
 struct queue *create_queue(int max_capacity) {
-
+    struct queue *q = malloc(sizeof(struct queue));
+    q->capacity = max_capacity;
+    q->front = 0;
+    q->size = 0;
+    q->arr = malloc(max_capacity * sizeof(int));
+    q->back = max_capacity - 1;
+    return q;
 }
 
 void enqueue(struct queue *q, int elem) {
@@ -9,5 +15,9 @@ void enqueue(struct queue *q, int elem) {
 }
 
 int dequeue(struct queue *q) {
+    return 0;
+}
 
+int main() {
+    
 }
--- queue.h
+++ queue.h
@@ -6,7 +6,7 @@
 struct queue {
     int front, back, size, capacity;
     int *arr;
-}
+};
 struct queue *create_queue(int max_capacity);
 void enqueue(struct queue *q, int elem);
 int dequeue(struct queue *q);
