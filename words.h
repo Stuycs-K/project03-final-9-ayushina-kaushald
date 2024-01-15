9a50df26168a0bb54130af23eb21be011577cb8f
DeanKaushal <deankaushal33@gmail.com>
2024-01-14 23:29:38 -0500

    updated the words.h file with the new methods

--- words.h
+++ words.h
@@ -9,15 +9,20 @@
 #include <sys/sem.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
+#include <time.h>
+#include "queue.h"
 
 #ifndef WORDS_H
 #define WORDS_H
 #define FILE_NAME "words"
 #define BUFFER_SIZE 1024
 #define SHM_KEY 0xdeadbeef
+char *generateLetter();
+int checkWord(char *word, char letter);
 void file_setup();
 void add_word(char *word);
 int find_word(char *word);
 int shm_setup();
+int wordValid(char * word, char letter);
 void remove_shm();
 #endif
